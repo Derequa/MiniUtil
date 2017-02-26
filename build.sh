6d38c1e8334223c87bd6b9731bba40f9aa8ce702
Derek Batts <clone101.derek@gmail.com>
2017-02-26 02:20:25 -0500

    Tweak to build script.

--- build.sh
+++ build.sh
@@ -10,5 +10,9 @@ ninja makeTestRunner
 echo "Running tests..."
 ninja tests
 echo "Generating Documentation..."
+if [ ! -d ./doc ]; then
+    echo "Creating doc directory..."
+    mkdir doc
+fi
 javadoc -d doc -sourcepath src -subpackages derek.util:derek.util.graph
 echo "Done!"
